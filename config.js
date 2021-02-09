912d3092d3d8ee144e35e8b0f295b52f7807b434
Kenneth Carter <kxccarter@gmail.com>
2021-02-09 16:15:37 -0600

    new origin

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://graphqlClass:tSGZRLbWWjYWobpx@cluster0.u5wja.mongodb.net/merng?retryWrites=true&w=majority',
+  SECRET_KEY: 'omg its so secret',
+};

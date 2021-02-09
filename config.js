9d678cb9bad0fc7aa86fffdc2015df50e7b64c58
Kenneth Carter <kxccarter@gmail.com>
2021-02-09 16:49:34 -0600

    fix file structure

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  MONGODB:
-    'mongodb+srv://graphqlClass:tSGZRLbWWjYWobpx@cluster0.u5wja.mongodb.net/merng?retryWrites=true&w=majority',
-  SECRET_KEY: 'omg its so secret',
-};
